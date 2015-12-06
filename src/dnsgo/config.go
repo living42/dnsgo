1306c624c31a765a807feda36b7bd7a1703c48a4
Living42 <qzeqing@gmail.com>
2015-12-06 13:29:15 +0800

    format code

--- src/dnsgo/config.go
+++ src/dnsgo/config.go
@@ -1,99 +1,98 @@
 package main
 
 import (
-    "github.com/oschwald/geoip2-golang"
-    "gopkg.in/yaml.v2"
-    "io/ioutil"
-    "flag"
-    "fmt"
-    "os"
-    "strings"
-)
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 
+	"github.com/oschwald/geoip2-golang"
+	"gopkg.in/yaml.v2"
+)
 
 const (
-    TCP_QUERY = "tcp"
-    UDP_QUERY = "udp"
-    POLICY_INCLUDED = "included"
-    POLICY_EXCLUDED = "excluded"
+	TCP_QUERY       = "tcp"
+	UDP_QUERY       = "udp"
+	POLICY_INCLUDED = "included"
+	POLICY_EXCLUDED = "excluded"
 )
 
-
 type Configuration struct {
-    Debug           bool
-    Listen          string          `yaml:"listen"`
-    ListenTCP       bool            `yaml:"listen_tcp"`
-    ListenUDP       bool            `yaml:"listen_udp"`
-    Resolvers       []*Resolver     `yaml:"resolvers"`
-    GeoIPDBPath     string          `yaml:"geoip_db"`
-    GeoipDB         *geoip2.Reader  `yaml:"-"`
-    cpuprofile string               `yaml:"-"`
-    memprofile string               `yaml:"-"`
+	Debug       bool
+	Listen      string      `yaml:"listen"`
+	ListenTCP   bool        `yaml:"listen_tcp"`
+	ListenUDP   bool        `yaml:"listen_udp"`
+	Resolvers   []*Resolver `yaml:"resolvers"`
+	GeoIPDBPath string      `yaml:"geoip_db"`
+	geoipDB     *geoip2.Reader
+	cpuprofile  string
+	memprofile  string
 }
 
-func DefaultConfig() *Configuration {
-    conf := new(Configuration)
-    conf.Debug = false
-    conf.Listen = "127.0.0.1:8053"
-    conf.ListenTCP = false
-    conf.ListenUDP = true
-    conf.GeoIPDBPath = ""
-    // TODO: default config
-    return conf
+func defaultConfig() *Configuration {
+	conf := new(Configuration)
+	conf.Debug = false
+	conf.Listen = "127.0.0.1:8053"
+	conf.ListenTCP = false
+	conf.ListenUDP = true
+	conf.GeoIPDBPath = ""
+	// TODO: default config
+	return conf
 }
 
-var conf = DefaultConfig()
+var conf = defaultConfig()
 var initialized bool
 
 func GetConfig() (*Configuration, error) {
-    if initialized {
-        return conf, nil
-    }
-    cpuprofile := flag.String("cpuprofile", "", "write cpu profile to file")
-    memprofile := flag.String("memprofile", "", "write memory profile to this file")
-    confi_path := flag.String("config", "dnsgo.yml", "load config")
-    flag.Usage = func() {
-        flag.PrintDefaults()
-    }
-    flag.Parse()
+	if initialized {
+		return conf, nil
+	}
+	cpuprofile := flag.String("cpuprofile", "", "write cpu profile to file")
+	memprofile := flag.String("memprofile", "", "write memory profile to this file")
+	configPath := flag.String("config", "dnsgo.yml", "load config")
+	flag.Usage = func() {
+		flag.PrintDefaults()
+	}
+	flag.Parse()
 
-    if *cpuprofile != "" {
-        conf.cpuprofile = *cpuprofile
-    }
-    if *memprofile != "" {
-        conf.memprofile = *memprofile
-    }
+	if *cpuprofile != "" {
+		conf.cpuprofile = *cpuprofile
+	}
+	if *memprofile != "" {
+		conf.memprofile = *memprofile
+	}
 
-    buffer, err := ioutil.ReadFile(*confi_path)
-    if err != nil {
-        return conf, err
-    }
-    if err := yaml.Unmarshal(buffer, conf); err != nil {
-        return conf, err
-    }
-    if conf.Debug {
-        s, _ := yaml.Marshal(conf)
-        fmt.Println("-----------config-start-----------")
-        fmt.Fprintln(os.Stderr, string(s))
-        fmt.Println("-----------config-stop-----------")
-    }
-    // TODO: validate config
-    if conf.Debug {
-        fmt.Println("opening geoipmmdb.")
-    }
-    conf.GeoipDB, err = geoip2.Open(conf.GeoIPDBPath)
-    if err != nil {
-        return conf, err
-    }
+	buffer, err := ioutil.ReadFile(*configPath)
+	if err != nil {
+		return conf, err
+	}
+	if err := yaml.Unmarshal(buffer, conf); err != nil {
+		return conf, err
+	}
+	if conf.Debug {
+		s, _ := yaml.Marshal(conf)
+		fmt.Println("-----------config-start-----------")
+		fmt.Fprintln(os.Stderr, string(s))
+		fmt.Println("-----------config-stop-----------")
+	}
+	// TODO: validate config
+	if conf.Debug {
+		fmt.Println("opening geoipmmdb.")
+	}
+	conf.geoipDB, err = geoip2.Open(conf.GeoIPDBPath)
+	if err != nil {
+		return conf, err
+	}
 
-    for _, servers := range conf.Resolvers {
-        for i, server := range servers.Server {
-            if !strings.Contains(server, ":") {
-                servers.Server[i] = server + ":53"
-            }
-        }
+	for _, servers := range conf.Resolvers {
+		for i, server := range servers.Server {
+			if !strings.Contains(server, ":") {
+				servers.Server[i] = server + ":53"
+			}
+		}
 
-    }
-    initialized = true
-    return conf, nil
+	}
+	initialized = true
+	return conf, nil
 }
--- src/dnsgo/main.go
+++ src/dnsgo/main.go
@@ -1,85 +1,85 @@
 package main
 
 import (
-    "github.com/miekg/dns"
-    "os"
-    "os/signal"
-    "log"
-    "fmt"
-    "runtime/pprof"
-    "syscall"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"runtime/pprof"
+	"syscall"
+
+	"github.com/miekg/dns"
 )
 
 func main() {
-    conf, err := GetConfig()
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+	conf, err := GetConfig()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-    if conf.cpuprofile != "" {
-        f, err := os.Create(conf.cpuprofile)
-        if err != nil {
-            log.Fatal(err)
-        }
-        log.Println("starting cpuprofile")
-        pprof.StartCPUProfile(f)
-        defer pprof.StopCPUProfile()
-    }
+	if conf.cpuprofile != "" {
+		f, err := os.Create(conf.cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("starting cpuprofile")
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
 
-    if conf.memprofile != "" {
-        log.Println("starting memprofile")
-        defer func() { 
-            f, err := os.Create(conf.memprofile)
-            if err != nil {
-                log.Fatal(err)
-            }        
-            pprof.WriteHeapProfile(f)
-        }()
-    }
+	if conf.memprofile != "" {
+		log.Println("starting memprofile")
+		defer func() {
+			f, err := os.Create(conf.memprofile)
+			if err != nil {
+				log.Fatal(err)
+			}
+			pprof.WriteHeapProfile(f)
+		}()
+	}
 
-    dns.HandleFunc(".", HandleQuestion)
-    if conf.ListenTCP {
-        go serve("tcp", conf.Listen)
-    }
-    if conf.ListenUDP {
-        go serve("udp", conf.Listen)
-    }
-    sig := make(chan os.Signal)
-    signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	dns.HandleFunc(".", handleQuestion)
+	if conf.ListenTCP {
+		go serve("tcp", conf.Listen)
+	}
+	if conf.ListenUDP {
+		go serve("udp", conf.Listen)
+	}
+	sig := make(chan os.Signal)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
 
 forever:
-    for {
-        select {
-        case s:= <-sig:
-            fmt.Printf("Signal (%d) received, stopping\n", s)
-            break forever
-        }
-    }
+	for {
+		select {
+		case s := <-sig:
+			fmt.Printf("Signal (%v) received, stopping\n", s)
+			break forever
+		}
+	}
 }
 
-func HandleQuestion(w dns.ResponseWriter, m *dns.Msg) {
-    conf, _ := GetConfig()
+func handleQuestion(w dns.ResponseWriter, m *dns.Msg) {
+	conf, _ := GetConfig()
 
+	log.Printf("question %s %s from %s\n", m.Question[0].Name, dns.TypeToString[m.Question[0].Qtype], w.RemoteAddr())
 
-    log.Printf("question %s %s from %s\n", m.Question[0].Name, dns.TypeToString[m.Question[0].Qtype], w.RemoteAddr())
-    
-    resultch := make(chan *dns.Msg)
+	resultch := make(chan *dns.Msg)
 
-    for _, resolver := range conf.Resolvers {
-        go resolver.Resolve(m, resultch)
-    }
-    
-    r := <- resultch
-    r.SetRcode(m, r.Rcode)
-    w.WriteMsg(r)
+	for _, resolver := range conf.Resolvers {
+		go resolver.Resolve(m, resultch)
+	}
+
+	r := <-resultch
+	r.SetRcode(m, r.Rcode)
+	w.WriteMsg(r)
 }
 
 func serve(net string, listen string) {
-    server := &dns.Server{Addr: listen, Net: net, TsigSecret: nil}
-    log.Printf("%s server listen on %s\n", net, listen)
-    err := server.ListenAndServe()
-    if err != nil {
-        log.Printf("Failed to setup the "+net+" server: %s\n", err.Error())
-    }
+	server := &dns.Server{Addr: listen, Net: net, TsigSecret: nil}
+	log.Printf("%s server listen on %s\n", net, listen)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Printf("Failed to setup the "+net+" server: %s\n", err.Error())
+	}
 }
